7da05eb2a4d8b08724d6d2a4c8aa1fb48ee239c5
YARIMIZU Misato <21716105@jwu.ac.jp>
2019-09-30 16:44:46 +0900

    calc(1+2+3).py

--- calc.py
+++ calc.py
@@ -1,4 +1,9 @@
 def calc(s):
-    return int(s)
+    print('s=',s)
+    nums = map(int, s.split('+'))
+    print('nums=',nums)
+    return sum(nums)
 
 print(calc("1"))
+print(calc("1+2"))
+print(calc("1+2+3"))
