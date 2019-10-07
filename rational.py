d6b3883c7d1aab5d9cc8118d17c1d65b14adb4be
YARIMIZU Misato <21716105@jwu.ac.jp>
2019-10-07 16:50:35 +0900

    addaddadd

--- rational.py
+++ rational.py
@@ -1,9 +1,12 @@
+import math
+
 class Q(object):
-    def __init__(self,a,b):
-        self.a = a
-        self.b = b
+    def __init__(self,a,b=1):
+        gcd = math.gcd(a,b)
+        self.a = a//gcd
+        self.b = b//gcd
     def __repr__(self):
-        if self.b ==1:
+        if self.b == 1:
             return str(self.a)
         return f'{self.a}/{self.b}'
     def __add__(self,q):
@@ -16,3 +19,6 @@ class Q(object):
 q1 = Q(1,2)
 q2 = Q(1,3)
 print(q1 + q2)
+print(q1 - q2)
+print(q1 * q2)
+print(q1 / q2)
