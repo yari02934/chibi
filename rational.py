235c78b534d788038a2a4242d4624f7f847e2f78
YARIMIZU Misato <21716105@jwu.ac.jp>
2019-10-07 16:15:02 +0900

    add inf

--- rational.py
+++ rational.py
@@ -3,7 +3,9 @@ class Q(object):
         self.a = a
         self.b = b
     def __repr__(self):
+        if self.b ==1:
+            return str(self.a)
         return f'{self.a}/{self.b}'
 
-q = Q(1,2)
+q = Q(3)
 print(q)
