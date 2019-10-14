5e621c4a987b215f68f2974ca9b46244ae5c4fc3
YARIMIZU Misato <21716105@jwu.ac.jp>
2019-10-14 15:43:29 +0900

    return

---
+++ exp.py
@@ -0,0 +1,18 @@
+
+class Val(object):
+    __slot__ = ['value']
+    def __init__(self, value = 0):
+            self.value =value
+    def __repr__(self):
+        return f'Val({self.value})'
+    def eval(self):
+        return
+
+v = Val(1)
+print(V)
+assert v.eval() == 1
+
+
+
+print()
+print()
