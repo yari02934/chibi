bfaa5aaa800f424e03ced96234bf7c98d598afa9
YARIMIZU Misato <21716105@jwu.ac.jp>
2019-10-14 15:57:39 +0900

    add

--- exp.py
+++ exp.py
@@ -12,6 +12,22 @@ v = Val(1)
 print(V)
 assert v.eval() == 1
 
+class Add(object):
+    __slots__=['left','right']
+    def __init__(self, a,b):
+        self.left = a
+        self.right＝ｂ
+    def eval (self):
+        return self.left.eval() + self.right.eval()
+
+v = Add(Val(1),Val(2))
+print(e.eval())
+assert v.eval()  == 3
+
+
+
+e = Add(Val(1),Add(Val(2),Val(3)))
+print(e.eval())
 
 
 print()
