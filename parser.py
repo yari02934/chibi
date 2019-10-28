f1daffe68638cb27bc649d937b019cbfe7d0b016
YARIMIZU Misato <21716105@jwu.ac.jp>
2019-10-28 16:50:03 +0900

    daa

--- parser.py
+++ parser.py
@@ -1,31 +1,18 @@
-from exp import Val, Add
+from exp import Val, Add,Sub,Mul,Div
 
-'''
 def parse(s: str):
-    num = int(s)
-    return Val(num)
-
-e = parse("123")
-print(e)
-
-s = "123+456"
-pos = s.find('+') #＋記号を探す
-print('pos',pos)
-
-s1 = s[0:pos]
-s2 = s[pos+1:]
-print(s, s1, s2) #+記号で分割
-'''
-
-ef parse(s: str):
-    pos = s.find('+')
-    if pos == -1:
-        num = int(s)
-        return Val(num)
-    else:
+   if s.find('+') > 0:
+       pos = s.find('*')
+       s1 = s[0:pos]
+       s2 = s[pos+1:]
+       return Add(parse(s1), parse(s2))
+   if s.find('*') > 0:
+        pos = s.find('*')
         s1 = s[0:pos]
         s2 = s[pos+1:]
-        return Add(Val(int(s1))), Val(int(s2))
+        return Mul(parse(s1), parse(s2))
+    return Val(int(s))
 
-e = parse("123+456+789")
-print(e)
+e = parse("1-2-3")
+print(e, e.eval())
+assert e.eval() == -4
